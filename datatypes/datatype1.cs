12e53b58859ddbceb9a57910fe37bc7a942c2a78
narmada2601 <narmada.fullstack@gmail.com>
2025-06-17 12:07:34 +0530

    DATATYPES

---
+++ datatypes/datatype1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+
+
+    class datatype1
+    {
+        static void Main(string[] args)
+        {
+            sbyte level = 12;
+            Console.WriteLine(level);//output-12 and  range -127 to 128 size=8bits.
+
+
+
+            short value = -1109;
+            Console.WriteLine(value);//o/p:1109 and range :: -32,768 to 32,767 size:16bits
+
+            int marks = 100;
+            Console.WriteLine(marks);//o/p:100 Range: -231 to 231 - 1 Size: 32 bits
+
+            long range = -1234456666L;
+            Console.WriteLine(range);//o/p:-1234456666 Range: -263 to 263-1 size:64 bits
+
+            //unsigned Integrals
+
+            byte age = 15;
+            Console.WriteLine(age);//Range: 0 to 255.
+
+            ushort value1 = 42019;
+            Console.WriteLine(value1);//Range:0 to 65,535
+
+            uint totalScore = 1151092;
+            Console.WriteLine(totalScore);// 0 to 232-1
+
+            ulong range1 = 17091821871L;
+            Console.WriteLine(range1);//0 to 264-1
+
+            float number = 43.27F;
+            Console.WriteLine(number);//1.5 × 10−45 to 3.4 × 1038
+
+            double value2 = -11092.53D;
+            Console.WriteLine(value2);//5.0 × 10−324 to 1.7 × 10308
+
+            char ch1 = '\u0042';
+            char ch2 = 'x';
+            Console.WriteLine(ch1);//b-unicode value
+            Console.WriteLine(ch2);//x
+
+            decimal bankBalance = 5005.25m;//[M at the end represent the value is of decimal type]
+            Console.WriteLine(bankBalance);
+
+            // boolean data type
+            bool b = true;
+
+            if (b == true)
+                Console.WriteLine("Hi narmada");
+
+            // To prevent the screen from running and closing quickly 
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+
+
---
+++ datatypes/datatype2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+
+class datatype2
+    {
+        static void Main()
+        {
+        Console.WriteLine(".........To find the min and maxium values........");
+        Console.WriteLine($"Byte => Minimum Range:{byte.MinValue} and Maximum Range:{byte.MaxValue}");
+        Console.WriteLine($"Integer => Minimum Range:{int.MinValue} and Maximum Range:{int.MaxValue}");
+        Console.WriteLine($"Float => Minimum Range:{float.MinValue} and Maximum Range:{float.MaxValue}");
+        Console.WriteLine($"Long => Minimum Range:{long.MinValue} and Maximum Range:{long.MaxValue}");
+        Console.WriteLine($"Double => Minimum Range:{double.MinValue} and Maximum Range:{double.MaxValue}");
+      
+
+        Console.WriteLine("...........to Get the size...............");
+        Console.WriteLine($"Size of Byte: {sizeof(byte)}");
+        Console.WriteLine($"Size of Integer: {sizeof(int)}");
+        Console.WriteLine($"Size of Character: {sizeof(char)}");
+        Console.WriteLine($"Size of Float: {sizeof(float)}");
+        Console.WriteLine($"Size of Long: {sizeof(long)}");
+        Console.WriteLine($"Size of Double: {sizeof(double)}");
+        Console.WriteLine($"Size of Bool: {sizeof(bool)}");
+     
+
+
+        Console.WriteLine("...........Char type...............");
+        char ch = 'B';
+        Console.WriteLine($"Char: {ch}");
+        Console.WriteLine($"Char Minimum: {(int)char.MinValue} and Maximum: {(int)char.MaxValue}");
+        Console.WriteLine($"Char Size: {sizeof(char)} Byte");
+      
+
+        Console.WriteLine("**************String type*********************");
+        string str = "Narmada";
+        Console.WriteLine("string length  is" +str.Length);
+         Console.WriteLine($"str Value: {str}");
+        Console.WriteLine("***readLine waits for the user to enter the name **********");
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+
+
+        Console.WriteLine("**************maximum digits ********************");
+        //testing 
+        float a = 1.7898638087f; //7 digits Maximum
+        double b = 1.789863808300294; //15 digits Maximum
+        decimal c = 1.78986380830029492956829m; //29 digits Maximum
+        Console.WriteLine("float value is "+a);
+        Console.WriteLine("Double value is " + b);
+        Console.WriteLine("Decimal  value is " + c);
+
+        Console.WriteLine("**************lets check the performance now*********************");
+
+        
+
+        Console.WriteLine("Checking the performance");
+          Stopwatch stopwatch1 = new Stopwatch();
+        stopwatch1.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            short s1 = 100;
+            short s2 = 100;
+            short s3 = 100;
+        }
+        stopwatch1.Stop();
+        Console.WriteLine($"short took : {stopwatch1.ElapsedMilliseconds} MS");
+        Stopwatch stopwatch2= new Stopwatch();
+        stopwatch2.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            decimal d1 = 100;
+            decimal d2 = 100;
+            decimal d3 = 100;
+        }
+        stopwatch1.Stop();
+        Console.WriteLine($"Decimal took : {stopwatch2.ElapsedMilliseconds} MS");
+        Console.ReadKey();
+        //here short took 1msec and decimal took 3 msec.
+
+
+
+
+
+
+    }
+}
